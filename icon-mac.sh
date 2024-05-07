a231a823a221cff830114b362316f6c516c2fddb
MXPSQL <73059135+MXP2095onetechguy@users.noreply.github.com>
2024-05-07 14:01:42 +0700

    Mac icon builder

--- icon-mac.sh
+++ icon-mac.sh
@@ -5,12 +5,26 @@
 # Copy icon here
 cp ./src/assets/image/icon.png ./icon.gitignored.png
 
-# Create an icon mapping sheet
+# Create an icon mapping sheet for automation purposes
+# Format - Filename=Size
 cat > iconmap.gitignored.txt << EOF
-i=e
+icon_16x16.png=16x16
+icon_16x16@2x.png=32x32
+icon_32x32.png=32x32
+icon_32x32@2x.png=64x64
+icon_128x128.png=128x128
+icon_128x128@2x.png=256x256
+icon_256x256.png=256x256
+icon_256x256@2x.png=512x512
+icon_512x512.png=512x512
+icon_512x512@2x.png=1024x1024
 EOF
 
 # Make iconset directory
-mkdir -p iconset.gitignored.icnset
+mkdir -p iconset.gitignored.iconset
 
-iconutil -c icns iconset.gitignored.icnset
+while IFS="=" read -r filenam size; do
+    magick convert ./icon.gitignored.png -resize $size "./iconset.gitignored.iconset/$filenam"
+done < iconmap.gitignored.txt
+
+iconutil -c icns iconset.gitignored.iconset icon.icns
